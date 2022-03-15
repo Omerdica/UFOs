18b1ba06116dd271b1bb6a3892745f919a479997
Omerdica <95953920+Omerdica@users.noreply.github.com>
2022-03-15 10:26:14 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -65,7 +65,7 @@ function updateFilters() {
   }
   
     if (filters["city"]) {
-      filtersData = filtersData.filter(dataRow => dataRow.city == filters["city"]);
+      filtersData = filtersData.filter(dataRow => dataRow.city == filters["City"]);
 
   }
     if (filters["state"]) {
